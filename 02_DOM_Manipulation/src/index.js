f5db6bdbaa23b36ea7fb3f11bb69adb6fb68f30a
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-01-31 14:28:26 -0700

    lesson 2 updates

--- 02_DOM_Manipulation/src/index.js
+++ 02_DOM_Manipulation/src/index.js
@@ -1,14 +1,52 @@
 //BookStore has been moved to data.js 
 console.log(bookStore);
+const bookList = document.querySelector("#book-list");
 
 function formatPrice(price) {
   return '$' + Number.parseFloat(price).toFixed(2);
 }
 
+function renderHeader(bookStore) {
+  document.querySelector('#store-name').textContent = bookStore.name;
+}
+renderHeader(bookStore);
+
+function renderFooter(bookStore) {
+  document.querySelector('#location').textContent = bookStore.location; 
+  document.querySelector('#number').textContent = bookStore.number; 
+  document.querySelector('#hours').textContent = bookStore.hours; 
+  document.querySelector('#address').textContent = bookStore.address; 
+}
+renderFooter(bookStore);
+
 // create a function called renderBook(book)
 // it will take a book object as an argument
 // and create the html struture for rendering 
 // that book and insert it into our webpage!
+function renderBook(book) {
+const li = document.createElement('li');
+li.className = "list-li";
+const h3 = document.createElement('h3');
+h3.textContent = book.title;
+li.append(h3);
+const pAuthor = document.createElement('p');
+pAuthor.textContent = book.author;
+li.append(pAuthor);
+const pPrice = document.createElement('p');
+pPrice.textContent = formatPrice(book.price);
+li.append(pPrice);
+const img = document.createElement('img');
+img.src = book.imageUrl;
+img.alt = `${book.title} cover`;
+li.append(img);
+const button = document.createElement('button');
+bookList.append(li);
+}
+
+bookStore.inventory.forEach(book => renderBook(book));
+// const book = bookStore.inventory[0];
+// renderBook(bookStore);
+
 
 // function renderBook(book) {
 // should create an li element that looks something like this:
